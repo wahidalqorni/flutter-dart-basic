0a71547bf1b3dd405dbc229c077d8f9fb70554ce
wahidalqorni <idunalqorni@gmail.com>
2022-06-25 15:05:34 +0700

    setget1

--- materi5/3gettersetter.dart
+++ materi5/3gettersetter.dart
@@ -1,67 +1,61 @@
 void main(){
   /**
-   * constructor => metehod khusus yg bisa digunakan untuk inisialisasi nilai object
+   * setter n getter
+   * seeter => suatu method yg berfungsi untuk mengeset atau mengubah suatu nilai dari atribut yg ada di class
+   * getter => suatu method yg berfungsi untuk mengambil nilai dari sesuatu yang diset
    */
 
-  // merealisasikan class RekeningBank menjadi object agar apa2 yg ada di kelas RekeningBank bisa dipake
-  // tulis nama class sesuai nama classny NamaClass objectCLass = new NamaClass()
-  RekeningBank rekeningBank = RekeningBank("Wahid", "BSA", 9999999999999);
-  Sekolah sekolah = Sekolah(lokasiSekolah: "Palembang", namaSekolah: "Sekolahan");
-  
-  // memanggil apa2 yg ada di kelas RekeningBank yg telah diobjeckkan menjadi rekeningBank
-  print(rekeningBank.namaBank);
-  rekeningBank.namaBank = "AAAA";
-  print(rekeningBank.namaBank);
+  // panggil kelas untuk dijadikan sebuah object
+  // tipe namaKelas = NamaKelas
+  RekeningBank rekeningBank = RekeningBank();
+  print("Nilai awal.....");
   print(rekeningBank.namaPemilik);
-  print(rekeningBank.saldo);
-  rekeningBank.label = "BBB";
-
-  print(sekolah.namaSekolah);
- 
+  print(rekeningBank._saldo);
+  print("Nilai diset");
+  rekeningBank.setNamaPemilik = "Person";
+  print("Nilia setelah diset");
+  print(rekeningBank.getNamaPemilik);
+  rekeningBank.setSaldo = 1000000;
+  print(rekeningBank.getSaldo);
+  print(rekeningBank._saldo);
+  // rekeningBank._saldo = -1;
+  // print(rekeningBank._saldo);
+  
 }
 
 
 class RekeningBank{
-  String namaPemilik = "";
-  String namaBank= "";
-  int saldo = 0;
-  String label = "";
 
-  // tambahkan constructor pd class RekeningBank
-  // constructor wajib / positioned parameter
-  RekeningBank(this.namaPemilik, this.namaBank, this.saldo );
+  // variabel/atribut yg bersifat public
+  String? namaPemilik;
+  String? namaBank;
+  int? _saldo;
 
-  cekSaldo(){
-    print("Saldo saat ini ${saldo}");
+  // variabel/atribut yg bersifat private
+  // ditandai dengan _
+  String? _kode;
+
+  // setter
+  set setNamaPemilik(String nama){
+    namaPemilik = nama;
   }
 
-  transfer(){
-    print("Fungsi transfer dijalankan");
+  // getter
+  get getNamaPemilik{
+    return namaPemilik;
   }
 
-  ambilSaldo(int jumlah){
-    int sisa = saldo - jumlah;
-    print("Ambil saldo sebesar ${jumlah} \nsisa saldo : ${sisa} ");
+
+  set setSaldo(int saldoInputan){ 
+    if(saldoInputan > 0 ) { // beri settingan pada inputan yg akan diinput nantinya agar tidak asal2an
+      _saldo = saldoInputan;
+    } else {
+      _saldo = 0;
+    }
   }
-}
 
-class Archer{
-  String ultimate ="";
-  String shoot ="";
-
-  // method
-
-}
-
-class Sekolah{
-  String? namaSekolah;
-  String? lokasiSekolah;
-  int? jumlahSiswa;
-  // constructor tidak wajib diisikan saat kelas dipanggil / named paramater
-  Sekolah({
-    this.namaSekolah,
-    this.lokasiSekolah,
-    this.jumlahSiswa
-  });
+  get getSaldo{
+    return _saldo;
+  }
 
 }
